85ea3cc8dfcb530e48a5b4524b6825e203161706
Paul Catalan <pjcatalan@gmail.com>
2022-08-25 21:00:26 +0000

    updated crio install

--- scripts/init_config.sh
+++ scripts/init_config.sh
@@ -8,20 +8,9 @@ sudo swapoff -a
 (crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
 
 ## Install CRI-O
-
-# Set ENV Variables
-OS="xUbuntu_22.04"
-VERSION="1.24"
-
-echo "deb [signed-by=/usr/share/keyrings/libcontainers-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" > sudo /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-
-echo "deb [signed-by=/usr/share/keyrings/libcontainers-crio-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" > sudo /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
-
-curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
-
-sudo apt-get update
-sudo apt-get install cri-o cri-o-runc
+sudo apt update
+sudo apt install -y curl jq
+curl https://raw.githubusercontent.com/cri-o/cri-o/main/scripts/get | sudo bash
 
 sudo systemctl daemon-reload
 sudo systemctl enable crio --now
